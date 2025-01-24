e22bc3a73079c0e887391ea78020411a7a01f459
felix923 <felixlopuran100@gmail.com>
2025-01-24 12:06:50 +0300

    Contact us page modified

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Particle from "../Particle";
-
 function Contact(){
     return(      
         <div className="Form" style={{marginTop: "100px", color: "red", marginBottom: "500px"}}>
           <Particle />
           <strong>Coming Soon!!</strong> 
+          
         </div>
     )
 }
