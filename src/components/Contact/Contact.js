e107b6ecbb1f2142f35404d5eca59436a6268a31
felix923 <felixlopuran100@gmail.com>
2025-01-24 09:33:03 +0300

    added particle component to contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Particle from "../Particle";
 
 function Contact(){
-    return(
+    return(      
         <div className="Form" style={{marginTop: "100px", color: "red", marginBottom: "500px"}}>
+          <Particle />
           <strong>Coming Soon!!</strong> 
         </div>
     )
